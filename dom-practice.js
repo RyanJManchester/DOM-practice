10f0a574d189b6ddd99557fd0468ef5e47c2cce3
RyanJManchester <ryanjamesmanchester@gmail.com>
2021-08-30 16:14:43 +1200

    first commit

---
+++ dom-practice.js
@@ -0,0 +1,18 @@
+const p = document.createElement('p');
+p.style.color = 'red';
+p.textContent = "Hey im red!";
+main.appendChild(p);
+const Haa3 = document.createElement('h3');
+Haa3.style.color = 'blue';
+Haa3.textContent = 'Im a blue H3!';
+main.appendChild(Haa3);
+const content = document.createElement('div');
+content.style.backgroundColor = 'pink';
+content.style.border = "solid black";
+main.appendChild(content);
+let h1 = document.createElement('h1');
+h1.textContent = "Im in a div";
+let p2 = document.createElement('p');
+p2.textContent = "ME TOO!";
+content.appendChild(h1);
+content.appendChild(p2);
